14eb0a0eed43bd82cb9624f156c85ff3fae90045
David <David@DESKTOP-46N0UF5>
2022-11-07 08:42:07 +0100

    Ultima versión del proyecto de ejemplo

--- src/main/java/test/DepartamentoRepository.java
+++ src/main/java/test/DepartamentoRepository.java
@@ -1,8 +1,12 @@
-
 package test;
 
 import entities.Departamento;
+import entities.Empleado;
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,6 +34,44 @@ public class DepartamentoRepository {
 
         Departamento departamento = entityManager.createNamedQuery("Departamento.busquedaPorNombre", Departamento.class).setParameter("dnombre", name).getSingleResult();
         return departamento != null ? Optional.of(departamento) : Optional.empty();
+    }
+
+    public void showNative() {
+        List<Departamento> depar = entityManager.createNativeQuery(
+                "SELECT dept_no, dnombre, loc FROM DEPART", Departamento.class)
+                .getResultList();
+
+        for (Departamento d : depar) {
+            String id = d.getDeptNo() + "";
+            String name = d.getDnombre();
+            System.out.println("Dept_no: " + id + " Nombre:" + name);
+        }
+    }
+
+    public void showJPQL() {
+        Query q = entityManager.createQuery("from Departamento as d where d.deptNo in (10,20,30) OR loc LIKE 'VAL%'");
+        List<Departamento> depar = q.getResultList();
+        for (Departamento d : depar) {
+            String id = d.getDeptNo() + "";
+            String name = d.getDnombre();
+            System.out.println("Dept_no: " + id + " Nombre:" + name);
+        }
+    }
+
+    public void showCriteria() {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Departamento> criteria = builder.createQuery(Departamento.class);
+        Root<Departamento> root = criteria.from(Departamento.class);
+        criteria.select(root);
+        criteria.where(builder.between(root.get("deptNo"), 10, 30)); // deptNo between (10,30)
+
+        List<Departamento> depar = entityManager.createQuery(criteria).getResultList();
+        for (Departamento d : depar) {
+            String id = d.getDeptNo() + "";
+            String name = d.getDnombre();
+            System.out.println("Dept_no: " + id + " Nombre:" + name);
+        }
     }
 
     public Optional<Departamento> save(Departamento departamento) {
